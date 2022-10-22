28d19c170ef3c6c114308c607ba6ac9fcbd02b22
trdthg <69898423+trdthg@users.noreply.github.com>
2022-10-22 06:13:31 +0000

    complete all execises

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,19 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+mod cc {
+    #[macro_export]
+    macro_rules! my_macro {
+        ($val: expr) => {
+            if $val == "world!" {
+                "Hello world!"
+            } else {
+                "Hello goodbye!"
+            }
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests {
